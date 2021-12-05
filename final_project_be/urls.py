702333e9a1a7d12abe5ffd3870c9cd10b0c38e77
marcellinuselbert11 <marcellinuselbert46@gmail.com>
2021-12-06 00:36:35 +0700

    change urls

--- final_project_be/urls.py
+++ final_project_be/urls.py
@@ -18,5 +18,5 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('showcase/',include('showcase.urls'))
+    path('',include('showcase.urls'))
 ]
