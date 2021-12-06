3050f96eb39b61400f1550ebc8573ac52dd3a8fe
plattycelly <87021641+plattycelly@users.noreply.github.com>
2021-12-07 00:20:04 +0700

    To Do 1 selesai (#7)
    Kiram

--- showcase/urls.py
+++ showcase/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
   path('<int:id>', views.FilmDetail.as_view()),
   path('<str:sort>', views.FilmSort.as_view()),
   
-  path('<int:id>/<str:action>',views.FilmLikeDislike.as_view())
+  path('<int:id>/<str:action>',views.FilmLikeDislike.as_view()),
+  path('search/',views.FilmSearchFilter.as_view())
 ]
